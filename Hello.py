1966a5b0e9954045e47dc2ac5a8aff6db4a5ce32
Guimill <86968380+Guimill@users.noreply.github.com>
2024-03-11 15:14:25 +0000

    test osm

--- Hello.py
+++ Hello.py
@@ -20,6 +20,8 @@ from streamlit.logger import get_logger
 import streamlit_folium as stf
 import pandas as pd
 import re
+import requests
+import json
 
 LOGGER = get_logger(__name__)
 
@@ -30,6 +32,8 @@ def remove_special_chars(city):
 
 Cities['city'] = Cities['city'].apply(remove_special_chars)
 Cities['city'] = Cities['city'].str.lower()
+
+overpass_url = "https://overpass-api.de/api/"
 
 def run():
     st.set_page_config(
@@ -46,15 +50,45 @@ def run():
 
     citie_selector = st.text_input('Name a city !', 'Paris')
     citie_selector = str.lower(citie_selector)
-
     city_lat = Cities.loc[Cities['city'] == citie_selector, 'lat'].iloc[0]
     city_lng = Cities.loc[Cities['city'] == citie_selector, 'lng'].iloc[0]
 
+    radius = 0.01  # 1 degree is approximately 111 kilometers
 
+    # Calculate bounding box coordinates
+    min_lat = city_lat - radius
+    max_lat = city_lat + radius
+    min_lng = city_lng - radius
+    max_lng = city_lng + radius
 
-    m = stf.folium.Map(location=[city_lat, city_lng], zoom_start=9)
+    # Bounding box format: (min_lat, min_lng, max_lat, max_lng)
+    bbox = (min_lat, min_lng, max_lat, max_lng)
 
-    radius = 100
+    overpass_query = f"""
+        [out:json];
+        (
+        way["highway"="cycleway"]({bbox});
+        way["highway"="primary"]({bbox});
+        way["highway"="motorway"]({bbox});
+        way["building"="historic"]({bbox});
+        way["waterway"="river"]({bbox});
+        way["natural"="wetland"]({bbox});
+        way["natural"="water"]({bbox});
+        relation["waterway"="river"]({bbox});
+        relation["natural"="wetland"]({bbox});
+        relation["natural"="water"]({bbox});
+        );
+        (._;);
+        out geom;
+        >;
+        out skel qt;
+    """
+
+    response = requests.get(overpass_url, params={'data': overpass_query})
+
+    m = stf.folium.Map(location=[city_lat, city_lng], zoom_start=15)
+
+    radius = 50
     stf.folium.CircleMarker(
         location=[city_lat, city_lng],
         radius=radius,
@@ -65,10 +99,12 @@ def run():
         opacity=1,
         popup="{} pixels".format(radius),
         tooltip="I am in pixels",
-    ).add_to(m)
+        ).add_to(m)
 
     stf.folium_static(m)
 
 
 if __name__ == "__main__":
     run()
+
+
