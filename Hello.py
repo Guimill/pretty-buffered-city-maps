b2c39523eaff23f5bf75af2c2a189e1f126eedfc
Guimill <86968380+Guimill@users.noreply.github.com>
2024-05-24 14:39:32 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -117,7 +117,7 @@ def run():
 
     with Image.open(io.BytesIO(img_data)) as im:
         # Define the cropping coordinates
-        left, upper, right, lower = 200, 100, 1000, 1000
+        left, upper, right, lower = 400, 100, 1000, 1000
     
         # Crop the image
         im_crop = im.crop((left, upper, right, lower))
